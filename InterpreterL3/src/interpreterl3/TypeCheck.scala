0ae7bca4b6a45840e736e8e135f5996134bc4242
Guilherme Leon Berno de Jesus <guileon1234@gmail.com>
2012-11-23 20:55:03 -0200

    avancado

--- InterpreterL3/src/interpreterl3/TypeCheck.scala
+++ InterpreterL3/src/interpreterl3/TypeCheck.scala
@@ -7,9 +7,9 @@ package interpreterl3
 
 
 abstract class TypeOperand
-case class B_() extends TypeOperand
-case class N_() extends TypeOperand
-case class Fn_ (T1 : TypeOperand, T2 : TypeOperand) extends TypeOperand
+case class Bool_() extends TypeOperand
+case class Int_() extends TypeOperand
+case class Func(T1 : TypeOperand, T2 : TypeOperand) extends TypeOperand
 case class Variable(v : Int) extends TypeOperand
 
 abstract class TypeEquation(op1 : TypeOperand, op2: TypeOperand);
@@ -31,7 +31,7 @@ object TypeCheck {
           var T1 : Variable = Variable(newInt())
           var T2 : Variable = Variable(newInt())
            (typecheck(e2)._t1,
-            TypeEquation(typecheck(e1)._1,B_()) ++ // tipo de e1 == bool
+            TypeEquation(typecheck(e1)._1,Bool_()) ++ // tipo de e1 == bool
             TypeEquation(T1,T2) ++ // T1==T2
             TypeEquation(T1,typecheck(e2)._1) ++ // tipo e2 = T1
             TypeEquation(T2,typecheck(e3)._1) ++ // tipo e3 = T2
@@ -40,6 +40,50 @@ object TypeCheck {
             typecheck(e3)._2
            )
         }
+      case Op(op,e1,e2)=>
+        {
+          op match{
+            case Plus =>
+              {
+                    var T1 : Variable = Variable(newInt())
+                    var T2 : Variable = Variable(newInt())
+                    (Int(),
+                      TypeEquation(T1,Int_()) ++
+                      TypeEquation(T2,Int_()) ++
+                      typecheck(e1)._2 ++
+                      typecheck(e2)._2
+                    )
+                    
+              }
+            case LargerOrEqual =>
+              {
+                     var T1 : Variable = Variable(newInt())
+                      var T2 : Variable = Variable(newInt())
+                    (Bool(),
+                      TypeEquation(T1,Bool_()) ++
+                      TypeEquation(T2,Bool_()) ++
+                      typecheck(e1)._2 ++
+                      typecheck(e2)._2
+                     )
+              }
+          }
+        }
+      case App(e1,e2)=>
+         {
+            var T1 : Variable = Variable(newInt())
+            var T2 : Variable = Variable(newInt())
+            (T2,
+             TypeEquation(Func(T1,T2),typecheck(e1)._1) ++
+             TypeEquation(T1,typecheck(e2)._1) ++
+             typecheck(e1)._2 ++
+             typecheck(e2)._2
+             
+            )
+            
+         }
+      case N => Int_()
+      case B => Bool_()
+      case Fn(x,e) => 
     }
   }
        
