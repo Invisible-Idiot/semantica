27c355e4a9b38b4643b1d0976908e827554061ac
Lucas <mestrecabecao@hotmail.com>
2012-11-30 23:01:26 -0200

    Teste pra ver se pega no tranco

---
+++ InterpreterL3/test/tests/EmptyTest.scala
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tests
+
+import org.junit._
+import Assert._
+
+class EmptyTest {
+
+  @Before
+  def setUp: Unit = {
+  }
+
+  @After
+  def tearDown: Unit = {
+  }
+
+  @Test
+  def example = {
+  }
+
+}
